3b45afa799fd2dbfc7cab51026065c676e111de7
holman-rd <63613297+holman-rd@users.noreply.github.com>
2021-11-04 16:39:11 -0500

    Componentes
    Creación de modelo, formulario request y  controlador

---
+++ app/Http/Controllers/VehiculoController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Categoria;
+use Illuminate\Support\Facades\Redirect;
+use App\Http\Requests\VehiculoFormRequest;
+use DB;
+
+class VehiculoController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+    public function index(Request $request)
+    {
+
+        $vehiculos=DB::table('vehiculo')
+            ->orderBy('id_vehiculo','desc')
+            ->paginate(7);
+
+        return view('parqueadero.vehiculos.index',["vehiculos"=>$vehiculos]);
+    }
+   
+
+    public function create()
+    {
+        return view("parqueadero.vehiculos.create");
+    }
+    public function store (VehiculoFormRequest $request)
+    {
+        $vehiculo = new Vehiculo;
+		$vehiculo->placa=$request->get('placa');
+		$vehiculo->telefono=$request->get('telefono');
+		$vehiculo->color=$request->get('color');
+		$vehiculo->estado=$request->get('estado');
+        return Redirect::to('parqueadero/vehiculo');
+
+    }
+
+
+    public function edit($id)
+    {
+        return view("parqueadero.vehiculos.edit",["vehiculo"=>Vehiculo::findOrFail($id)]);
+    }
+
+
+    public function update(CategoriaFormRequest $request,$id)
+    {
+        $vehiculo=Vehiculo::findOrFail($id);
+        $vehiculo = new Vehiculo;
+		$vehiculo->placa=$request->get('placa');
+		$vehiculo->telefono=$request->get('telefono');
+		$vehiculo->color=$request->get('color');
+		$vehiculo->estado=$request->get('estado');
+        $vehiculo->update();
+        return Redirect::to('parqueadero/vehiculo');
+    }
+    public function destroy($id)
+    {
+        $vehiculo=Vehiculo::findOrFail($id);
+
+        $vehiculo->delete();
+        return Redirect::to('parqueadero/vehiculo');
+    }
+
+
+
+
+
+}
--- app/Vehiculo.php
+++ app/Vehiculo.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehiculo extends Model
 {
-
+    protected $table = 'vehiculo';
+    protected $primaryKey='id_vehiculo';
+    public $timestamps =false;
+    
+    protected $fillable=['placa', 'telefono', 'color','estado'];
+    protected $guarded=[];
 }
